9709cca3c9c21205d39a0ec6eb4bed423629a21f
RBerberat <robin.berberat@gmail.com>
2020-03-31 12:20:58 +0200

    some bs

--- stuff.py
+++ stuff.py
@@ -90,71 +90,28 @@ def create_meaning_vector_from_im(im, alpha=0.85):
     foo = create_lm_from_im(im)
     foo = foo*alpha
 
-    bar = np.ones((len(foo), len(foo)), dtype=float)
-    bar = bar/len(foo)
+    length = len(foo)
+    bar = np.ones((length, length), dtype=float)
+    bar = bar/length
     bar = bar*(1-alpha)
 
-    baz = foo+bar
+    baz = foo+bar #G
     print("baz", baz)
 
     bomb = np.zeros((len(baz), len(baz)), dtype=float)
     for i,s in enumerate(bomb):
-        bomb[i][i] = 1
+        bomb[i][i] = 1.0 #En
 
     print("bomb", bomb)
 
-    baz = baz-bomb
+    baz = baz-bomb #G-En
     print("baz", baz)
 
-    nullvector = np.zeros(len(baz), dtype=float)
-    print("nullvector", nullvector)
+    print("stuffy", np.zeros((1,3), dtype=float))
+    print("sol", np.linalg.solve(baz, np.zeros((3), dtype=float)))
 
-    solution = np.linalg.inv(baz)
-    print(solution.astype(float))
-    print()
-
-    print(baz*solution)
 
     return None
 
-def guassian_algorithm(A):
-    """ Return Row Echelon Form of matrix A """
-
-    # if matrix A has no columns or rows,
-    # it is already in REF, so we return itself
-    r, c = A.shape
-    if r == 0 or c == 0:
-        return A
-
-    # we search for non-zero element in the first column
-    for i in range(len(A)):
-        if A[i, 0] != 0:
-            break
-    else:
-        # if all elements in the first column is zero,
-        # we perform REF on matrix from second column
-        B = guassian_algorithm(A[:,1:])
-        # and then add the first zero-column back
-        return np.hstack([A[:,:1], B])
-
-    # if non-zero element happens not in the first row,
-    # we switch rows
-    if i > 0:
-        ith_row = A[i].copy()
-        A[i] = A[0]
-        A[0] = ith_row
-
-    # we divide first row by first element in it
-    A[0] = A[0] / A[0,0]
-    # we subtract all subsequent rows with first row (it has 1 now as first element)
-    # multiplied by the corresponding element in the first column
-    A[1:] -= A[0] * A[1:,0:1]
-
-    # we perform REF on matrix from second row, from second column
-    B = guassian_algorithm(A[1:,1:])
-
-    # we add first row and first (zero) column, and return
-    return np.vstack([A[:1], np.hstack([A[1:,:1], B]) ])
-
-E = setupMatrix2()
+E = setupMatrix3()
 create_meaning_vector_from_im(E)
